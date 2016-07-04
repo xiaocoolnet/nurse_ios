c17e9482eb57525dbaadd150b7e43c5175e997a1
shenxiaolong <852700979@qq.com>
2016-07-04 10:16:10 +0800

    7.4 commit

---
+++ HSHW_2016/HSPostResumeView.swift
@@ -0,0 +1,15 @@
+//
+//  HSPostResumeView.swift
+//  HSHW_2016
+//
+//  Created by xiaocool on 16/7/2.
+//  Copyright © 2016年 校酷网络科技公司. All rights reserved.
+//
+
+import UIKit
+
+class HSPostResumeView: UIView {
+    
+    
+    
+}
---
+++ HSHW_2016/HSStateCommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  HSStateCommentCell.swift
+//  HSHW_2016
+//
+//  Created by xiaocool on 16/7/3.
+//  Copyright © 2016年 校酷网络科技公司. All rights reserved.
+//
+
+import UIKit
+
+class HSStateCommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
