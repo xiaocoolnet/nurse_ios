d1b677ed53766d9a12ef0a6fb297b784826771d3
zxxmengmei <157577331@qq.com>
2016-10-25 15:11:53 +0800

    bug修复

---
+++ Pods/Target Support Files/Pods-HSHW_2016/Pods-HSHW_2016-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HSHW_2016 : NSObject
+@end
+@implementation PodsDummy_Pods_HSHW_2016
+@end
---
+++ Pods/Target Support Files/Pods-HSHW_2016/Pods-HSHW_2016-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HSHW_2016VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HSHW_2016VersionString[];
+
